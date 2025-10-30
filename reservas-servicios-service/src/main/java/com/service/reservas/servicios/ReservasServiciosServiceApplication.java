e1e4a28044f268b5d0075bc9b69bf774e8584d66
sfcg2 <castillo.sofia@correounivalle.edu.co>
2025-10-30 11:13:28 -0500

    "Estructura proyecto"

---
+++ reservas-servicios-service/src/main/java/com/service/reservas/servicios/ReservasServiciosServiceApplication.java
@@ -0,0 +1,13 @@
+package com.service.reservas.servicios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservasServiciosServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservasServiciosServiceApplication.class, args);
+	}
+
+}
